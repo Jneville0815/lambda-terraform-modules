a6d237bbef0479188f22c7a879dd306787985719
Jimmy Neville <jimmyeneville@gmail.com>
2022-12-21 19:31:01 -0500

    Modify script

--- lambdas/quote_text/quote_text.py
+++ lambdas/quote_text/quote_text.py
@@ -1,5 +1,6 @@
 import os
-from botocore.vendored import requests
+import urllib3
+import json
 from twilio.rest import Client
 
 
@@ -9,16 +10,15 @@ def lambda_handler(event, context):
     twilio_number = os.environ['TWILIO_PHONE_NUMBER']
 
     client = Client(account_sid, auth_token)
+    http = urllib3.PoolManager()
+    res = http.request('GET', 'https://3cl6ts0f6f.execute-api.us-east-1.amazonaws.com/production/api/userInfo/jimmyeneville@gmail.com/getQuote')
+    json_res = json.loads(res.data.decode('utf-8'))
 
-    res = requests.get('https://3cl6ts0f6f.execute-api.us-east-1.amazonaws.com/production/api/userInfo/jimmyeneville@gmail.com/getQuote')
+    message = client.messages \
+        .create(
+        body=f"{json_res['source']}: {json_res['quote']}",
+        from_=twilio_number,
+        to='+1XXXXXXXXXX'
+    )
 
-    print(res)
-
-    # message = client.messages \
-    #     .create(
-    #     body="This is a test",
-    #     from_=twilio_number,
-    #     to='+1XXXXXXXXXX'
-    # )
-    #
-    # print(f"Text message sent (Message: {message.body}. Error: {message.error_message})")
+    print(f"Text message sent (Message: {message.body}. Error: {message.error_message})")
